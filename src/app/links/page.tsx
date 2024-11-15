46cdb1f55f78348993f7d01278e89cf948ed71ac
Dommik <dominik.strnadel@gmail.com>
2024-11-15 09:54:14 +0100

    feat: loading

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,11 +1,8 @@
 import LinkList from "~/components/LinkList";
-import { Suspense } from "react";
 
 export default async function Links() {
 
   return(
-    <Suspense fallback={<div>Loading...</div>}>
       <LinkList/>
-    </Suspense>
   )
 }
