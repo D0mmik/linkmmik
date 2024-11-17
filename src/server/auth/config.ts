34b2ae6233c896e04593a692063a0aaca805af19
Dominik Strnadel <dominik.strnadel@gmail.com>
2024-11-17 21:06:48 +0100

    feat: google auth

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "~/server/db";
 import {
@@ -41,6 +42,10 @@ export const authConfig = {
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? ""
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? ""
     }),
   ],
   adapter: DrizzleAdapter(db, {
